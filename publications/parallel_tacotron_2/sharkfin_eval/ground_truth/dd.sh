79780703b9bcf4ce689ef12bb987d17daaa1d96b
isaaceelias <72845550+isaaceelias@users.noreply.github.com>
2021-03-31 17:15:02 +0300

    some audio samples for Parallel Tacotron 2

---
+++ publications/parallel_tacotron_2/sharkfin_eval/ground_truth/dd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+fileutil cp /cns/is-d/home/isaace/brain/rs=6.3/new_sharkfin_eval_set/groundtruth_wav/hol_241_76107.wav hol_241_76107.wav 
+fileutil cp /cns/is-d/home/isaace/brain/rs=6.3/new_sharkfin_eval_set/groundtruth_wav/hol_291_89323.wav hol_291_89323.wav
+fileutil cp /cns/is-d/home/isaace/brain/rs=6.3/new_sharkfin_eval_set/groundtruth_wav/ioc_008_02450.wav ioc_008_02450.wav
+fileutil cp /cns/is-d/home/isaace/brain/rs=6.3/new_sharkfin_eval_set/groundtruth_wav/ioe_004_00905.wav ioe_004_00905.wav
+fileutil cp /cns/is-d/home/isaace/brain/rs=6.3/new_sharkfin_eval_set/groundtruth_wav/jmp_033_08732.wav jmp_033_08732.wav
+fileutil cp /cns/is-d/home/isaace/brain/rs=6.3/new_sharkfin_eval_set/groundtruth_wav/tfe_004_00153.wav tfe_004_00153.wav
+fileutil cp /cns/is-d/home/isaace/brain/rs=6.3/new_sharkfin_eval_set/groundtruth_wav/tfe_009_90094.wav tfe_009_90094.wav
+fileutil cp /cns/is-d/home/isaace/brain/rs=6.3/new_sharkfin_eval_set/groundtruth_wav/tme_037_90554.wav tme_037_90554.wav
